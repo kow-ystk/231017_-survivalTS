6df76c1d926b831154a593fac0d891366a2a02da
Koki Kamezono <kow1ag25@gmail.com>
2023-10-18 02:26:05 +0900

    ページを表示したときに画像を表示する

--- Next.jsで猫画像ジェネレーターを作ろう/random-cat/pages/index.tsx
+++ Next.jsで猫画像ジェネレーターを作ろう/random-cat/pages/index.tsx
@@ -1,7 +1,21 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const IndexPage: NextPage = () => {
-  return <div>猫画像予定地</div>;
+  // useStateを使って状態を定義する
+  const [imageUrl, setImageUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+  // マウント時に画像を読み込む宣言
+  useEffect(()=>{
+    fetchImage().then((newImage) => {
+      // 画像URLの状態を更新する
+      setImageUrl(newImage.url);
+      // ローディング状態を更新する
+      setLoading(false);
+    });
+  },[]);
+  // ローディング中でなければ画像を表示する
+  return <div>{loading || <img src={imageUrl} />}</div>;
 };
 
 export default IndexPage;
