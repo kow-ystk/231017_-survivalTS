a66c6c4d6398d36688993530581628b32e857c0d
Koki Kamezono <kow1ag25@gmail.com>
2023-10-18 12:33:42 +0900

    データフェッチAPIを使ってリクエスト時に初期画像を取得す

--- Next.jsで猫画像ジェネレーターを作ろう/random-cat/pages/index.tsx
+++ Next.jsで猫画像ジェネレーターを作ろう/random-cat/pages/index.tsx
@@ -1,19 +1,20 @@
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import {  GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
 
-const IndexPage: NextPage = () => {
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   // useStateを使って状態を定義する
-  const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [imageUrl, setImageUrl] = useState(initialImageUrl);
+  const [loading, setLoading] = useState(false);
+
   // マウント時に画像を読み込む宣言
-  useEffect(()=>{
-    fetchImage().then((newImage) => {
-      // 画像URLの状態を更新する
-      setImageUrl(newImage.url);
-      // ローディング状態を更新する
-      setLoading(false);
-    });
-  },[]);
+  // useEffect(()=>{
+  //   fetchImage().then((newImage) => {
+  //     // 画像URLの状態を更新する
+  //     setImageUrl(newImage.url);
+  //     // ローディング状態を更新する
+  //     setLoading(false);
+  //   });
+  // },[]);
 
   // ボタンをクリックしたときに画像を読み込む処理
   const handleClick =async () => {
@@ -36,6 +37,16 @@ const IndexPage: NextPage = () => {
 };
 
 export default IndexPage;
+
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> =async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    }
+  }
+};
 
 type Image = {
   url: string;
